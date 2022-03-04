e1bde7afdeacc927a201cf3984a4963dd1df11f1
aucomte <aurore.f.comte@gmail.com>
2022-03-04 15:17:48 +0100

    ajout script bash

---
+++ RNAja/scripts/launch_RNAja.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+############      SLURM CONFIGURATION      ###################
+#SBATCH --partition=highmemdell
+#SBATCH --nodelist=node30
+#SBATCH --job-name=RNAja
+#SBATCH --output=RNAja-out
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH --time=120:00:00
+
+############################################################
+
+# sbatch launch_RNAja.sh config.yaml
+
+# module load
+
+module load system/python/3.7.2
+module load system/singularity/3.6.0
+module load system/Miniconda3/1.0
+
+RNAja run_local -c ${CONFIG} -t 20
